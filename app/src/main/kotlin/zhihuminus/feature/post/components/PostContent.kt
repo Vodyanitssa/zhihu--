6596444ba9246a5e38ef27c8a254bb08b73fa8aa
cgy <guanyuchen.cs@gmail.com>
2026-08-25 14:39:16 +0800

    fix: make post content text selectable via SelectionContainer

--- app/src/main/kotlin/zhihuminus/feature/post/components/PostContent.kt
+++ app/src/main/kotlin/zhihuminus/feature/post/components/PostContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,7 +42,9 @@ fun PostContent(
                 }
             }
         } else {
-            RenderContentNodes(post.content)
+            SelectionContainer {
+                RenderContentNodes(post.content)
+            }
         }
 
         // Pin image gallery
